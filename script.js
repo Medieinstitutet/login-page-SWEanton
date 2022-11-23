47ea44d79a4edc88360bb6fe8184b3b1b6ca1665
Anton Henningsson <anton_9408@hotmail.com>
2022-11-23 16:06:06 +0100

    update

--- script.js
+++ script.js
@@ -1,23 +1,43 @@
-const loginBtn = document.getElementById('loginBtn');
-const signUpBtn = document.getElementById('signUpBtn');
-const userName = document.getElementById('userName');
-const password = document.getElementById('password');
-const demo = document.getElementById('demo');
+console.log("test");
+
+const loginBtn = document.getElementById("logInBtn");
+const userName = document.getElementById("userName");
+const password = document.getElementById("password");
+const SignUpBtn = document.getElementById("SignUpBtn");
+const demo = document.getElementById("demo");
+const LogOutBtn=document.getElementById("LogOutBtn");
 
 const users = [ 
     {"userName": "janne", "password": "test"},
-    {"userName": "anton", "kuken": "test"},
-
  ];
 
-log_in = () => { 
-    console.log('klick på knapp ');
+ loginBtn.addEventListener("click",() => {    
+    console.log("klick på knapp");
   
-    for (i = 0; i < users.length; i++) {
-      if (userName.value == users[i].userName && password.value == users[i].password) {
-        demo.innerHTML = "Du är inloggad som: " + userName.value;
-        return true;
-      }
-    }
-    demo.innerHTML = "Användaren finns inte";
+ for (i = 0; i < users.length; i++) {
+   if (userName.value == users[i].userName && password.value == users[i].password) {
+     demo.innerHTML = "Du är inloggad som: " + userName.value;
+     return true;
+   }
+ }
+ demo.innerHTML = "Användaren finns inte";
+
+ }  );
+// //!*
+// log_in = () => { 
+//     console.log("klick på knapp");
+  
+//     for (i = 0; i < users.length; i++) {
+//       if (userName.value == users[i].userName && password.value == users[i].password) {
+//         demo.innerHTML = "Du är inloggad som: " + userName.value;
+//         return true;
+//       }
+//     }
+//     demo.innerHTML = "Användaren finns inte";
+// }//!
+
+SignUpBtn.addEventListener("click"), () => {
+    console.log("Klick på knapp")
+
+        let 
 }
