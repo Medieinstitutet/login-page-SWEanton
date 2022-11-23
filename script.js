453d897e680ffaadc873ac8319f2e44e4c294f13
Anton Henningsson <anton_9408@hotmail.com>
2022-11-23 16:21:23 +0100

    updt3

--- script.js
+++ script.js
@@ -1,16 +1,30 @@
 console.log("test");
 
-const loginBtn = document.getElementById("logInBtn");
-const userName = document.getElementById("userName");
-const password = document.getElementById("password");
+const loginBtn = document.getElementById("LogInBtn");
+const userName = document.getElementById("UserName");
+const password = document.getElementById("Password");
 const SignUpBtn = document.getElementById("SignUpBtn");
-const demo = document.getElementById("demo");
+const demo = document.getElementById("Demo");
 const LogOutBtn=document.getElementById("LogOutBtn");
 
 const users = [ 
     {"userName": "janne", "password": "test"},
  ];
+ if (!localStorage.getItem("userName, password")) {
+    fetch(".json")
+    .then(res => res.json())
+    .then(data => {
+        // console.log("fetch json", data);
 
+        // let stringData = JSON.stringify(data);
+        // console.log("stringified", stringData);
+
+        // let parsedData = JSON.parse(stringData);
+        // console.log("parsed data", parsedData);
+
+        localStorage.setItem("persons", JSON.stringify(data))
+    })
+}
  loginBtn.addEventListener("click",() => {    
     console.log("klick på knapp");
   
