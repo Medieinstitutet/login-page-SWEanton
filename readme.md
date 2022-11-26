79ee18f70e22f4b0090abda28e709e56ea27ce2f
Anton Henningsson <anton_9408@hotmail.com>
2022-11-27 00:41:01 +0100

    added a readme.md modified html css & js

---
+++ readme.md
@@ -0,0 +1,4 @@
+Squid Game 2024 application, 
+user: janne pw: test
+
+
--- script.js
+++ script.js
@@ -8,28 +8,26 @@ const logInBtn = document.getElementById("logInBtn");
 const logInPage = document.getElementById("logInPage");
 const logOutPage = document.getElementById("logOutPage");
 
-const user = [
-    {username:"janne", password:"test"},
-    {username:"anton", password:"kuken"},
+let users = [
+    {userName:"janne", password:"test"},
 ]
+var personsRegister =
+
 logInBtn.addEventListener("click", () => { 
   let personsRegister = JSON.parse(localStorage.getItem("personsRegister"));
   for (i = 0; i < personsRegister.length; i++) {
     if (usernameInput.value == personsRegister[i].username && passwordInput.value == personsRegister[i].password) {
       logInPage.style.display = "none"
-      demo.innerHTML = ""
       logOutPage.style.display = "block";
       demo.innerHTML = "Best of luck: " + usernameInput.value;
       demo.style.color = "red"
       return true;
   } else {
-    demo.innerHTML = "You have been carefully selected for this and we've already given you the right log in credentials. Please use them, this is NOT a joke. ";
+    demo.innerHTML = "You have been carefully selected for this and we've already given you the correct credentials. Please use them, this is NOT a joke. ";
     demo.style.color = "red";
   }
   }
 })
-
-newFunction();
 
         const logOut = document.getElementById("logOut");
         let logOutBtn = document.createElement("button");
@@ -55,18 +53,17 @@ function newFunction() {
 
 var personsRegister = localStorage.getItem('userName', 'password');
 console.log(personsRegister); //  To print the value of localStorage variable name
-1
-2
+
 var name = localStorage.getItem('name');
 // Set localStorage item
-localStorage.setItem('dataObject', JSON.stringify(userName));
+localStorage.setItem('userName', JSON.stringify(userName));
 var retrievedObject = localStorage.getItem('userName');
 
 // console.log retrieved item
 console.log('userName: ', JSON.parse(userName));
 
 // Set localStorage item
-localStorage.setItem('dataObject', JSON.stringify(password));
+localStorage.setItem('password', JSON.stringify(password));
 var retrievedObject = localStorage.getItem('password');
 
 // console.log retrieved item
@@ -74,18 +71,18 @@ console.log('password: ', JSON.parse(password));
 
 
 
-localStorage.setItem('dataObject', JSON.stringify(userName));
-var retrievedObject = localStorage.getItem('userName');
+localStorage.setItem('userName', JSON.stringify(userName));
+var retrievedObject = localStorage.getItem("userName");
 
 // console.log retrieved item
-console.log('userName: ', JSON.parse(userName));
+console.log("userName: ", JSON.parse(userName));
 
 // Set localStorage item
-localStorage.setItem('dataObject', JSON.stringify(usernameInput));
-var retrievedObject = localStorage.getItem('password');
+localStorage.setItem('usernameInput', JSON.stringify(usernameInput));
+var retrievedObject = localStorage.getItem("password");
 
 // console.log retrieved item
-console.log('userName: ', JSON.parse(userName));
+console.log("userName: ", JSON.parse(usernameInput));
 
 
 alert( localStorage.usernameInput );
