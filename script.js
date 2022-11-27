94b745bb674245ced4dacc359bf850714539245a
Anton Henningsson <anton_9408@hotmail.com>
2022-11-27 20:18:47 +0100

    added cookies

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
+alert:("this is a not a real squidgame application")
+
 console.log("test");
 
+const cookie = document.getElementById("cookie");
 const demo = document.getElementById("demo");
 const $ = document.getElementById("$");
 const header = document.getElementById("header");
 const footer = document.getElementById("footer");
-const mainBox = document.getElementById("mainBox");
 const userID = document.getElementById("usernameBox");
 const userPassword = document.getElementById("passwordBox");
 const placeholderBtn = document.getElementById("placeholderBtn");
+const mainBox = document.getElementById("mainBox");
+
+
 
 let credentials = [{
     userName: "janne",
     password: "test" ,
     }];
-
 
 function checkPassword() {  
     let person = credentials.find(person => person.userName === userID.value);
@@ -22,18 +26,15 @@ function checkPassword() {
         localStorage.setItem("userName", JSON.stringify(loginUser));
         toggleBtn();
         loggedIn();
-
     } else {
         demo.innerHTML = "Wrong credentials, please try again.  ";
         demo.style.font = "italic";
         demo.style.color = "red";
         demo.style.top = "20px";
-
     }
     userID.value="";
     userPassword.value="";
 }
-
 if (localStorage.getItem("userName")) { 
     alreadyLogIn();
     toggleBtn();
@@ -41,8 +42,6 @@ if (localStorage.getItem("userName")) {
     createLoginBtn();
     loggedout();
 } 
-
-
 function toggleBtn () {
     placeholderBtn.innerHTML = "";      
 
@@ -50,37 +49,30 @@ function toggleBtn () {
     placeholderBtn.appendChild(logoutBtn);
     logoutBtn.style.color = "red";
     logoutBtn.innerText = "log out";
-
-
     logoutBtn.addEventListener("click", () => { 
         mainBox.innerHTML= "";
         localStorage.removeItem("userName");
         createLoginBtn();        
     })         
 }
-
 function createLoginBtn () {    
     placeholderBtn.innerHTML ="";
     userID.style.display = "";
     userPassword.style.display = "";
-
     let loginBtn = document.createElement("button");
     placeholderBtn.appendChild(loginBtn);
     loginBtn.style.color = "red";
     loginBtn.innerText = "Log in";
-
     loginBtn.addEventListener("click", () => {
         checkPassword();        
     })
 }
-
 function loggedIn() {    
     let welcomeBox = document.createElement("span");
     mainBox.append(welcomeBox);
     welcomeBox.className = "box";
     welcomeBox.insertAdjacentHTML("afterbegin", "You're now accepted to the 2024 Squid Game. User:  " +userID.value);
 }
-
 function alreadyLogIn() {
     let loggedInUser = JSON.parse(localStorage.getItem("userName"));
     let welcomeBox = document.createElement("span");
@@ -88,17 +80,11 @@ function alreadyLogIn() {
     welcomeBox.className = "box";
     welcomeBox.insertAdjacentHTML("afterbegin", "Hello again, did you change your mind? If so, please send 0.5 $BTC to #34xp4vRoCGJym3xR7yCVPFHoCNxv4Twseo. User: " +loggedInUser);
 }
-
-
 function loggedout() {
     mainBox.innerHTML= "";
 }
-
-
-
-
 function showPopUp() {
-    var cookie = $.cookie('the_cookie');
+    var cookie = $.cookie("the_cookie");
     if(!cookie){
         alert(" Please note, this is a joke");
         $.cookie('the_cookie', 'the_value');
